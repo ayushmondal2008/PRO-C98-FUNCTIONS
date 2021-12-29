8d0501e2e17e6106b8b913bb2472e41719a217a0
Ayush Mondal <75127242+ayushmondal2008@users.noreply.github.com>
2021-12-29 18:30:50 +0530

    Create swappingFile.py

---
+++ swappingFile.py
@@ -0,0 +1,24 @@
+def swapFileData():
+
+    file1 = input("Enter the first filename")
+    file2 = input("Enter the second filename")
+
+    with open(file1, 'r') as a :
+        data_a = a.read()
+        
+    with open(file2, 'r') as b :
+        data_b = b.read()
+    
+
+
+    with open(file1, 'w') as a :
+        a.write(data_b)
+        
+    with open(file2, 'w') as b :
+        b.write(data_a)
+
+    a.close()
+    b.close()
+
+    print("OPEN YOUR FILE AND CHECK THE CONTENT")
+swapFileData()
